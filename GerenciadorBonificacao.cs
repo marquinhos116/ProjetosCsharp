30010f29dfdb4fa38b2e9c994ad2cded2210fba1
marquinhos116 <54999404+marquinhos116@users.noreply.github.com>
2020-02-15 04:29:05 -0300

    Add files via upload

---
+++ GerenciadorBonificacao.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+   public class GerenciadorBonificacao
+    {
+
+        private double _TotalBonificacao; 
+        
+        public void Registrar(Funcionario funcionario)
+        {
+           _TotalBonificacao+= funcionario.GetBonificacao();
+        
+        }
+
+        public void Registrar(Diretor diretor)
+        {
+          
+            _TotalBonificacao +=diretor.GetBonificacao();
+
+        }
+
+
+
+
+
+        public double GetTotalBonificacao() 
+        {
+            return _TotalBonificacao;
+        
+        }
+
+        
+    
+    
+    }
+}
