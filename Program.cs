ea3acab3f7bf3ea112af008c30753aac55ce447a
marquinhos116 <54999404+marquinhos116@users.noreply.github.com>
2020-02-15 04:26:50 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿
+using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GerenciadorBonificacao Gerenciador = new GerenciadorBonificacao();
+            
+
+
+            
+            
+            
+            
+            
+            Funcionario Carlos = new Funcionario();
+
+            Carlos.Nome = "Carlos";
+            Carlos.CPF = "121.806.976-78";
+            Carlos.Salario = 2000;
+
+
+            Gerenciador.Registrar(Carlos);
+            
+
+            Diretor Roberta = new Diretor();
+
+            Roberta.Nome = "Roberta";
+            Roberta.CPF = "454.658.148-3";
+            Roberta.Salario = 5000;
+
+            Gerenciador.Registrar(Roberta);
+
+            Console.WriteLine(Carlos.Nome);
+            Console.WriteLine(Carlos.GetBonificacao());
+
+            Console.WriteLine(Roberta.Nome);
+            Console.WriteLine(Roberta.GetBonificacao());
+
+            Console.WriteLine("Total de Bonificações é = "  + Gerenciador.GetTotalBonificacao());
+
+
+
+            Console.ReadLine();
+
+
+        
+        }
+    }
+}
